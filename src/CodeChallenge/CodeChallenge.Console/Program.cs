e0cf011f7b64b07740b5124d02825590bf62391c
deadlydog <deadlydog@hotmail.com>
2024-08-23 14:16:48 -0600

    feat: Have console app use DanielSchroeder mappings instead of the default modulus mappings

--- src/CodeChallenge/CodeChallenge.Console/Program.cs
+++ src/CodeChallenge/CodeChallenge.Console/Program.cs
@@ -1,6 +1,12 @@
 using CustomerItems;
 
-var app = new ItemsRetriever();
+List<ItemMapping> DanielSchroederMappings = new()
+{
+	new ItemMapping(3, "Daniel"),
+	new ItemMapping(5, "Schroeder")
+};
+
+var app = new ItemsRetriever(new ModulusItemMapper(DanielSchroederMappings));
 var items = app.GetItems(int.MaxValue);
 
 foreach (string item in items)
