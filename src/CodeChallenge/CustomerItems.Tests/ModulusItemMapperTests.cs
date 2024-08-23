68b9f7e81e5051907b65589ecbcf2741b49da893
deadlydog <deadlydog@hotmail.com>
2024-08-23 14:33:30 -0600

    test: Add more tests to ensure order is preserved

--- src/CodeChallenge/CustomerItems.Tests/ModulusItemMapperTests.cs
+++ src/CodeChallenge/CustomerItems.Tests/ModulusItemMapperTests.cs
@@ -93,21 +93,161 @@ public class ModulusItemMapperTests
 		}
 	}
 
-	public class WhenUsingManyOrderedMappings()
+	public class WhenUsingManyAscendingOrderedMappings()
 	{
-		private List<ItemMapping> ManyOrderedMappings = new()
+		private List<ItemMapping> AscendingMappings = new()
 		{
 			new ItemMapping(3, "Three"),
 			new ItemMapping(5, "Five"),
 			new ItemMapping(7, "Seven"),
 			new ItemMapping(9, "Nine"),
-			new ItemMapping(11, "Eleven"),
-			new ItemMapping(13, "Thirteen"),
 		};
 
-		private ModulusItemMapper CreateSutWithDanielSchroederMappings()
+		private ModulusItemMapper CreateSutWithAscendingMappings()
 		{
-			return new ModulusItemMapper(ManyOrderedMappings);
+			return new ModulusItemMapper(AscendingMappings);
+		}
+
+		[Fact]
+		public void MappingAnItem_WhenNotDivisibleByAnyNumber_ShouldReturnTheNumber()
+		{
+			var sut = CreateSutWithAscendingMappings();
+			var input = 1;
+
+			var result = sut.MapItem(input);
+
+			result.Should().Be("1");
+		}
+
+		[Fact]
+		public void MappingAnItem_WhenDivisibleBy3_ShouldReturnThree()
+		{
+			var sut = CreateSutWithAscendingMappings();
+			var input = 3;
+
+			var result = sut.MapItem(input);
+
+			result.Should().Be("Three");
+		}
+
+		[Fact]
+		public void MappingAnItem_WhenDivisibleBy5_ShouldReturnFive()
+		{
+			var sut = CreateSutWithAscendingMappings();
+			var input = 5;
+
+			var result = sut.MapItem(input);
+
+			result.Should().Be("Five");
+		}
+
+		[Fact]
+		public void MappingAnItem_WhenDivisibleBy7_ShouldReturnSeven()
+		{
+			var sut = CreateSutWithAscendingMappings();
+			var input = 7;
+
+			var result = sut.MapItem(input);
+
+			result.Should().Be("Seven");
+		}
+
+		[Fact]
+		public void MappingAnItem_WhenDivisibleBy9_ShouldReturnThreeAndNine()
+		{
+			var sut = CreateSutWithAscendingMappings();
+			var input = 9;
+
+			var result = sut.MapItem(input);
+
+			result.Should().Be("Three Nine");
+		}
+	}
+
+	public class WhenUsingManyDescendingOrderedMappings()
+	{
+		private List<ItemMapping> DescendingMappings = new()
+		{
+			new ItemMapping(9, "Nine"),
+			new ItemMapping(7, "Seven"),
+			new ItemMapping(5, "Five"),
+			new ItemMapping(3, "Three"),
+		};
+
+		private ModulusItemMapper CreateSutWithDescendingMappings()
+		{
+			return new ModulusItemMapper(DescendingMappings);
+		}
+
+		[Fact]
+		public void MappingAnItem_WhenNotDivisibleByAnyNumber_ShouldReturnTheNumber()
+		{
+			var sut = CreateSutWithDescendingMappings();
+			var input = 1;
+
+			var result = sut.MapItem(input);
+
+			result.Should().Be("1");
+		}
+
+		[Fact]
+		public void MappingAnItem_WhenDivisibleByOnlyOneNumber_ShouldReturnThatNumbersMapping()
+		{
+			var sut = CreateSutWithDescendingMappings();
+			var input = 3;
+
+			var result = sut.MapItem(input);
+
+			result.Should().Be("Three");
+		}
+
+		[Fact]
+		public void MappingAnItem_WhenDivisibleByTwoNumbers_ShouldReturnBothNumbersInOrder()
+		{
+			var sut = CreateSutWithDescendingMappings();
+			var input = 9;
+
+			var result = sut.MapItem(input);
+
+			result.Should().Be("Nine Three");
+		}
+
+		[Fact]
+		public void MappingAnItem_WhenDivisibleByThreeNumbers_ShouldReturnAllNumbersInOrder()
+		{
+			var sut = CreateSutWithDescendingMappings();
+			var input = 63;
+
+			var result = sut.MapItem(input);
+
+			result.Should().Be("Nine Seven Three");
+		}
+	}
+
+	public class WhenUsingManyOutOfOrderMappings()
+	{
+		private List<ItemMapping> DescendingMappings = new()
+		{
+			new ItemMapping(7, "Seven"),
+			new ItemMapping(3, "Three"),
+			new ItemMapping(9, "Nine"),
+			new ItemMapping(5, "Five"),
+		};
+
+		private ModulusItemMapper CreateSutWithDescendingMappings()
+		{
+			return new ModulusItemMapper(DescendingMappings);
+		}
+
+		[Fact]
+		public void MappingAnItem_WhenDivisibleByMultipleNumbers_ShouldReturnAllNumbersInOrder()
+		{
+			var sut = CreateSutWithDescendingMappings();
+			var input = 945; // 7 * 3 * 9 * 5 = 945
+
+			var result = sut.MapItem(input);
+
+			result.Should().Be("Seven Three Nine Five");
 		}
 	}
 }
