d2595cdc6a28b8885535f6c75b73b29075c0baed
deadlydog <deadlydog@hotmail.com>
2024-08-23 14:16:19 -0600

    test: Add tests for default module mappings test: Refactor tests with different mappings into different classes

--- src/CodeChallenge/CustomerItems.Tests/ModulusItemMapperTests.cs
+++ src/CodeChallenge/CustomerItems.Tests/ModulusItemMapperTests.cs
@@ -4,78 +4,110 @@ namespace CustomerItems.Tests;
 
 public class ModulusItemMapperTests
 {
-	private List<ItemMapping> DanielSchroederMappings = new()
+	public class WhenUsingDefaultMappings()
 	{
-		new ItemMapping(3, "Daniel"),
-		new ItemMapping(5, "Schroeder")
-	};
+		[Theory]
+		[InlineData(int.MinValue)]
+		[InlineData(-101)]
+		[InlineData(-1)]
+		[InlineData(0)]
+		[InlineData(1)]
+		[InlineData(2)]
+		[InlineData(15)]
+		[InlineData(12345)]
+		[InlineData(int.MaxValue)]
+		public void MappingAnItem_WhenTheNumberIsPositiveOrNegative_ShouldReturnTheNumber(int number)
+		{
+			var sut = new ModulusItemMapper();
 
-	private ModulusItemMapper CreateSutWithDanielSchroederMappings()
-	{
-		return new ModulusItemMapper(DanielSchroederMappings);
+			var result = sut.MapItem(number);
+
+			result.Should().Be(number.ToString());
+		}
 	}
 
-	[Fact]
-	public void MappingAnItem_WhenNotDivisibleBy3Or5_ShouldReturnTheNumber()
+	public class WhenUsingDanielSchroederMappings()
 	{
-		var sut = new ModulusItemMapper();
-		var input = 1;
+		private List<ItemMapping> DanielSchroederMappings = new()
+		{
+			new ItemMapping(3, "Daniel"),
+			new ItemMapping(5, "Schroeder")
+		};
 
-		var result = sut.MapItem(input);
+		private ModulusItemMapper CreateSutWithDanielSchroederMappings()
+		{
+			return new ModulusItemMapper(DanielSchroederMappings);
+		}
 
-		result.Should().Be("1");
+		[Fact]
+		public void MappingAnItem_WhenNotDivisibleBy3Or5_ShouldReturnTheNumber()
+		{
+			var sut = CreateSutWithDanielSchroederMappings();
+			var input = 1;
+
+			var result = sut.MapItem(input);
+
+			result.Should().Be("1");
+		}
+
+		[Theory]
+		[InlineData(3)]
+		[InlineData(6)]
+		[InlineData(9)]
+		[InlineData(12)]
+		public void MappingAnItem_WhenDivisibleBy3AndNot5_ShouldReturnDaniel(int number)
+		{
+			var sut = CreateSutWithDanielSchroederMappings();
+
+			var result = sut.MapItem(number);
+
+			result.Should().Be("Daniel");
+		}
+
+		[Theory]
+		[InlineData(5)]
+		[InlineData(10)]
+		[InlineData(20)]
+		[InlineData(25)]
+		public void MappingAnItem_WhenDivisibleBy5AndNot3_ShouldReturnSchroeder(int number)
+		{
+			var sut = CreateSutWithDanielSchroederMappings();
+
+			var result = sut.MapItem(number);
+
+			result.Should().Be("Schroeder");
+		}
+
+		[Theory]
+		[InlineData(15)]
+		[InlineData(30)]
+		[InlineData(45)]
+		[InlineData(60)]
+		public void MappingAnItem_WhenDivisibleBy3And5_ShouldReturnDanielSchroeder(int number)
+		{
+			var sut = CreateSutWithDanielSchroederMappings();
+
+			var result = sut.MapItem(number);
+
+			result.Should().Be("Daniel Schroeder");
+		}
 	}
 
-	[Theory]
-	[InlineData(3)]
-	[InlineData(6)]
-	[InlineData(9)]
-	[InlineData(12)]
-	public void MappingAnItem_WhenDivisibleBy3AndNot5_ShouldReturnDaniel(int number)
+	public class WhenUsingManyOrderedMappings()
 	{
-		var sut = CreateSutWithDanielSchroederMappings();
+		private List<ItemMapping> ManyOrderedMappings = new()
+		{
+			new ItemMapping(3, "Three"),
+			new ItemMapping(5, "Five"),
+			new ItemMapping(7, "Seven"),
+			new ItemMapping(9, "Nine"),
+			new ItemMapping(11, "Eleven"),
+			new ItemMapping(13, "Thirteen"),
+		};
 
-		var result = sut.MapItem(number);
-
-		result.Should().Be("Daniel");
-	}
-
-	[Theory]
-	[InlineData(5)]
-	[InlineData(10)]
-	[InlineData(20)]
-	[InlineData(25)]
-	public void MappingAnItem_WhenDivisibleBy5AndNot3_ShouldReturnSchroeder(int number)
-	{
-		var sut = CreateSutWithDanielSchroederMappings();
-
-		var result = sut.MapItem(number);
-
-		result.Should().Be("Schroeder");
-	}
-
-	[Theory]
-	[InlineData(15)]
-	[InlineData(30)]
-	[InlineData(45)]
-	[InlineData(60)]
-	public void MappingAnItem_WhenDivisibleBy3And5_ShouldReturnDanielSchroeder(int number)
-	{
-		var sut = CreateSutWithDanielSchroederMappings();
-
-		var result = sut.MapItem(number);
-
-		result.Should().Be("Daniel Schroeder");
-	}
-
-	[Fact]
-	public void MappingAnItem_WhenItsLarge_ItDoesNotCrash()
-	{
-		var sut = new ModulusItemMapper();
-		var input = int.MaxValue;
-
-		var result = sut.MapItem(input);
-
-		result.Should().Be(int.MaxValue.ToString());
+		private ModulusItemMapper CreateSutWithDanielSchroederMappings()
+		{
+			return new ModulusItemMapper(ManyOrderedMappings);
+		}
 	}
 }
