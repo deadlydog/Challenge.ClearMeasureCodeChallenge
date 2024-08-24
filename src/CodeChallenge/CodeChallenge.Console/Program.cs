5cd4872049c3c1b49a5abf2ef860c2b419e9a671
deadlydog <deadlydog@hotmail.com>
2024-08-24 17:19:17 -0600

    feat: Update console app to also show how to provide your own custom item mapper

--- src/CodeChallenge/CodeChallenge.Console/Program.cs
+++ src/CodeChallenge/CodeChallenge.Console/Program.cs
@@ -6,10 +6,30 @@ List<ItemMapping> DanielSchroederMappings = new()
 	new ItemMapping(5, "Schroeder")
 };
 
-var app = new ItemsRetriever(new ModulusItemMapper(DanielSchroederMappings));
-var items = app.GetItems(int.MaxValue);
+var itemRetriever = new ItemsRetriever(new ModulusItemMapper(DanielSchroederMappings));
+var items = itemRetriever.GetItems(100000);
 
-foreach (string item in items)
+foreach (var item in items)
 {
 	Console.WriteLine(item);
+}
+
+Console.WriteLine("Press Enter to continue and see results of the first 100 results using the AlphabetItemMapper...");
+Console.ReadLine();
+
+var alphabetItemRetriever = new ItemsRetriever(new AlphabetItemMapper());
+var letters = alphabetItemRetriever.GetItems(100);
+
+foreach (var letter in letters)
+{
+	Console.WriteLine(letter);
+}
+
+public class AlphabetItemMapper : IMapItems
+{
+	public string MapItem(int number)
+	{
+		// Return the letter of the alphabet that corresponds to the number passed in.
+		return ((char)((number % 26) + 64)).ToString();
+	}
 }
